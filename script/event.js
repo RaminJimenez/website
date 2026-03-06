65f1dfdeb9cfa15505a95c083a4f583ebe90e0f5
RaminJimenez <noodler026@gmail.com>
2026-03-06 07:07:34 -0800

    dadaada

---
+++ script/event.js
@@ -0,0 +1,54 @@
+clickBox = document.querySelector('.click')
+spinBox = document.querySelector('.spin')
+moveBox = document.querySelector('.move')
+hoverBox = document.querySelector('.hover')
+dblclickBox = document.querySelector('.dblclick')
+
+colors = ['blue', 'green', 'purple', 'red']
+index = 0
+
+clickBox.addEventListener('click', () => {
+    clickBox.style.background = colors[index]
+    index = index + 1
+    if (index > 3)
+       index = 0
+
+    clickBox.style.color = 'white'
+})
+
+spinBox.addEventListener('click', () => {
+    spinBox.classList.toggle('start')
+})
+
+x = 0
+y = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        x = x + 10
+    }
+    if (event.key == 'ArrowUp') {
+        y = y - 10
+    }
+    if (event.key == 'ArrowDown') {
+        y = y + 10
+    }
+    if (event.key == 'ArrowLeft') {
+        x = x - 10
+    }
+    moveBox.style.transform = `translate(${x}px, ${y}px)`
+})
+
+hoverBox.addEventListener('mouseenter', () => {
+    hoverBox.style.height = "200px"
+    hoverBox.style.width = "300px"
+})
+
+hoverBox.addEventListener('mouseleave', () => {
+    hoverBox.style.height = "60px"
+    hoverBox.style.width = "250px"
+})
+
+dblClickBox.addEventListener('mouseclick', () => {
+    
+})
