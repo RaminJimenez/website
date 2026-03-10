0c110d28cf15d7065ed9059411ba4c12a946854d
RaminJimenez <noodler026@gmail.com>
2026-03-10 17:00:57 +0000

    Tue, Mar 10, 2026, 10:00 AM -07:00

--- script/interactive.js
+++ script/interactive.js
@@ -1,12 +1,18 @@
 clickBox = document.querySelector('.click')
 
-colors = ['blue', 'red', 'green', 'purple', 'magenta', 'orange']
-index = 0;
+colors1 = ['blue', 'red', 'green', 'purple', 'magenta', 'orange']
+colors2 = ['orange', 'green', 'blue', 'violet', 'turqoise', 'maroon']
+index1 = 0;
+index2 = 0;
 clickBox.addEventListener('click', () => {
-    clickBox.style.background = colors[index]
-    index = index + 1
-    if (index > 6)
-       index = 0
+    clickBox.style.background = colors1[index1]
+    index1 = index1 + 1
+    if (index1 > 6)
+       index1 = 0
 
     clickBox.style.color = 'white'
+    background.style.color = colors2[index2]
+    index2 = index2 + 2
+    if (index2 > 6)
+        index2 = 0
 })
