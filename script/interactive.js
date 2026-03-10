3b64b9e3893e6dfd5447803b3dbbd6a641547f9e
RaminJimenez <noodler026@gmail.com>
2026-03-10 16:48:54 +0000

    Tue, Mar 10, 2026, 9:48 AM -07:00

---
+++ script/interactive.js
@@ -0,0 +1,8 @@
+clickBox = document.querySelector('.click')
+
+colors = ['blue', 'red', 'green', 'purple', 'magenta', 'orange']
+index = 0;
+clickBox.addEventListener('click', () => {
+    clickBox.style.background = colors[index]
+    for ()
+})
