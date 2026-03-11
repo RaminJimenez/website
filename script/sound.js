5eb20589688acbfaabd1cb25bc2ccd1842dfc16e
RaminJimenez <noodler026@gmail.com>
2026-03-11 16:33:54 +0000

    Wed, Mar 11, 2026, 9:33 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,6 +1,11 @@
 buttons = document.querySelector('.buttons')
+applause = document.querySelector('.applause')
 
 btn = document.createElement('button')
-btn.innerText = "Click Me"
+btn.innerText = "applause"
 btn.classList.add('btn')
+btn.addEventListener('click', () => {
+    applause.play()
+
+})
 buttons.appendChild(btn)
