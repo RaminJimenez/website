8f9679095fb6433df7512abcaa7f68e8fd6a8b9e
RaminJimenez <noodler026@gmail.com>
2026-03-11 16:44:07 +0000

    Wed, Mar 11, 2026, 9:44 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -9,13 +9,13 @@ wrong = document.querySelector('.wrong')
 sounds ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 for (let i = 0; i < 6; i = i + 1){
-btn = document.createElement('button')
-btn.innerText = "sounds[i]"
-btn.classList.add('btn')
-btn.addEventListener('click', () => {
-    sounds[i].play()
+    btn = document.createElement('button')
+    btn.innerText = "sounds[i]"
+    btn.classList.add('btn')
+    btn.addEventListener('click', () => {
+        document.querySelector(`.${sounds[i]}`).play()
 
-})
-buttons.appendChild(btn)
+    })
+    buttons.appendChild(btn)
 }
 
