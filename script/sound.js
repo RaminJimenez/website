255aefbda70936ff7d6f2ff4840b17fc11364099
RaminJimenez <noodler026@gmail.com>
2026-03-11 16:45:23 +0000

    Wed, Mar 11, 2026, 9:45 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,16 +1,10 @@
 buttons = document.querySelector('.buttons')
-applause = document.querySelector('.applause')
-boo = document.querySelector('.boo')
-gasp = document.querySelector('.gasp')
-tada = document.querySelector('.tada')
-victory = document.querySelector('.victory')
-wrong = document.querySelector('.wrong')
 
-sounds ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
-for (let i = 0; i < 6; i = i + 1){
+for (let i = 0; i < 6; i = i + 1) {
     btn = document.createElement('button')
-    btn.innerText = "sounds[i]"
+    btn.innerText = sounds[i]
     btn.classList.add('btn')
     btn.addEventListener('click', () => {
         document.querySelector(`.${sounds[i]}`).play()
