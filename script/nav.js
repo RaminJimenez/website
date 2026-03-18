50e7e5edba19ca53182ff532ef32c3886934ff30
RaminJimenez <noodler026@gmail.com>
2026-03-17 20:07:31 -0700

    everything

---
+++ script/nav.js
@@ -0,0 +1,11 @@
+nav = document.querySelector('.nav')
+
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 420) {
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
+})
