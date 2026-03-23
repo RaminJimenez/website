dd42bf63c36989544387fde5cfda5ba59148d647
RaminJimenez <noodler026@gmail.com>
2026-03-23 16:53:59 +0000

    Mon, Mar 23, 2026, 9:53 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -30,6 +30,9 @@ function createInsect() {
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)">`
     game_container.appendChild(insect)
+    insect.addEventListener('click', () => {
+        catchInsect()
+    })
 }
 
 function getRandomLocation() {
