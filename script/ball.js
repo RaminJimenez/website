fcfe1550ebc8321f31dcba73ea11aa76737b50bf
RaminJimenez <noodler026@gmail.com>
2026-04-15 16:11:00 +0000

    Wed, Apr 15, 2026, 9:11 AM -07:00

---
+++ script/ball.js
@@ -0,0 +1,22 @@
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+
+const LPaddle = document.createElement('div')
+document.body.appendChild(All)
+
+
+
+
+
+let ballSpeed = 5
+let ballXDirection = 1
+let ballYDirection = 1
+
+
+
+function moveBall(){
+
+}
+
+let ballTop = ballYPosition
+let ballBottom = ballYPosition + 2 * 
